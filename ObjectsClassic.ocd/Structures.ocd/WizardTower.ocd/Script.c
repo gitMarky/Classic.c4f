b83e0cca5552979d5e02460cb4e2bc8dd01f8506
Mark <markh4b@hotmail.com>
2014-05-24 18:22:53 +0200

    Constantins wizard tower graphics implemented and some strings corrected

---
+++ ObjectsClassic.ocd/Structures.ocd/WizardTower.ocd/Script.c
@@ -0,0 +1,13 @@
+/*
+ *	Wizard Tower
+ *
+ *	Author: Marky, Mesh: constantin, Texture: constantin
+ */
+
+#include ClassicHutWooden
+
+local BlastIncinerate = 100;
+local HitPoints = 100;
+
+local Name="$Name$";
+local Description = "$Description$";
