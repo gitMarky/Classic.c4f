df54acf8898a03f9dbe529106c42c1c26b5aaf76
Mark <markh4b@hotmail.com>
2016-01-06 18:15:16 +0100

    Fix: Skin selection defaults to the actual clonk

--- Classic.ocf/Classic.ocd/Crew.ocd/Clonk.ocd/Script.c
+++ Classic.ocf/Classic.ocd/Crew.ocd/Clonk.ocd/Script.c
@@ -24,6 +24,42 @@ private func DiggingProcess()
 	return true;
 }
 
+func SetSkin(int new_skin)
+{
+	// Remember skin
+	skin = new_skin;
+
+	//Adventurer
+	if(skin == 0)
+	{	skin_name = nil;
+		gender = 0;	}
+
+	//Steampunk
+	if(skin == 1)
+	{	skin_name = "Steampunk";
+		gender = 1; }
+
+	//Alchemist
+	if(skin == 2)
+	{	skin_name = "Alchemist";
+		gender = 0;	}
+	
+	//Farmer
+	if(skin == 3)
+	{	skin_name = "Farmer";
+		gender = 1;	}
+		
+	var material_name = Format("Clonk%sClassic", skin_name);
+
+	SetGraphics(skin_name, Clonk);
+	SetMeshMaterial(material_name);
+	if (skin == 2) SetMeshMaterial(material_name, 1);
+	//RemoveBackpack(); //add a backpack
+	//AttachBackpack();
+	SetAction("Jump"); //refreshes animation
+
+	return skin;
+}
 
 
 local ActMap = {
