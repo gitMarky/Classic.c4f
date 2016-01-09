501b6026b1a0212c55c345ea95d7b2f2bb18784e
Mark <markh4b@hotmail.com>
2016-01-09 12:23:48 +0100

    Castle update

--- Classic.ocf/Classic.ocd/Structures.ocd/Castle.ocd/Script.c
+++ Classic.ocf/Classic.ocd/Structures.ocd/Castle.ocd/Script.c
@@ -10,6 +10,12 @@
 
 local tower;
 
+protected func Construction()
+{
+	_inherited(...);
+    this.PictureTransformation = Trans_Mul(Trans_Rotate(-20, 0, 1, 0), Trans_Rotate(-15, 1, 0, 0), Trans_Rotate(7, 0, 0, 1), Trans_Translate(10000, -5000, 30000), Trans_Scale(900, 900, 450));
+}
+
 protected func Initialize()
 {
 	tower = CreateObjectAbove(ClassicCastleTower, -23, +44, GetOwner());
@@ -37,7 +43,6 @@ local Description = "$Description$";
 local BlastIncinerate = 160;
 local ContainBlast = 1;
 local BurnTo = ClassicCastleBurned;
-local PictureTransformation = Trans_Mul(Trans_Rotate(-20, 0, 1, 0), Trans_Rotate(-15, 1, 0, 0), Trans_Rotate(7, 0, 0, 1), Trans_Translate(10000, -5000, 30000), Trans_Scale(900, 900, 450));
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //
@@ -50,32 +55,21 @@ OpenDoor = {
 	Procedure = DFA_NONE,
 	Length = 10,
 	Delay = 1,
-	X = 0,
-	Y = 87,
-	Wdt = 20,
-	Hgt = 22,
-	OffX = 40,
-	OffY = 65,
-	FacetBase = 1,
-	NextAction = "Door",
+	NextAction = "DoorOpen",
 	StartCall = "SoundOpenDoor",
+	Animation = "MainOpenDoor",
 },
 
-Door = {
+DoorOpen = {
 	Prototype = Action,
-	Name = "Door",
+	Name = "DoorOpen",
 	Procedure = DFA_NONE,
+	Length = 1,
 	Delay = 40,
-	X = 180,
-	Y = 87,
-	Wdt = 20,
-	Hgt = 22,
-	OffX = 40,
-	OffY = 65,
-	FacetBase = 1,
 	NextAction = "CloseDoor",
 	StartCall = "OpenEntrance",
 	EndCall = "CloseEntrance",
+	Animation = "MainDoorOpen",
 },
 
 CloseDoor = {
@@ -83,17 +77,44 @@ CloseDoor = {
 	Name = "CloseDoor",
 	Procedure = DFA_NONE,
 	Length = 10,
-	Reverse = 1,
 	Delay = 1,
-	X = 0,
-	Y = 87,
-	Wdt = 20,
-	Hgt = 22,
-	OffX = 40,
-	OffY = 65,
-	FacetBase = 1,
 	NextAction = "Idle",
 	StartCall = "SoundCloseDoor",
+	Animation = "MainCloseDoor",
+},
+
+TowerOpenDoor = {
+	Prototype = Action,
+	Name = "OpenDoor",
+	Procedure = DFA_NONE,
+	Length = 10,
+	Delay = 1,
+	NextAction = "TowerDoorOpen",
+	StartCall = "SoundOpenDoor",
+	Animation = "TowerOpenDoor",
+},
+
+TowerDoorOpen = {
+	Prototype = Action,
+	Name = "Door",
+	Procedure = DFA_NONE,
+	Length = 1,
+	Delay = 40,
+	NextAction = "TowerCloseDoor",
+	StartCall = "OpenEntrance",
+	EndCall = "CloseEntrance",
+	Animation = "TowerDoorOpen",
+},
+
+TowerCloseDoor = {
+	Prototype = Action,
+	Name = "CloseDoor",
+	Procedure = DFA_NONE,
+	Length = 10,
+	Delay = 1,
+	NextAction = "Idle",
+	StartCall = "SoundCloseDoor",
+	Animation = "TowerCloseDoor",
 },
 
 };
