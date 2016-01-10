47181baef0de5e890bac5191284e51e7a499f0fa
Mark <markh4b@hotmail.com>
2016-01-10 22:04:55 +0100

    Added content dumping from OC to lorry

--- Classic.ocf/Classic.ocd/Vehicles.ocd/Lorry.ocd/Script.c
+++ Classic.ocf/Classic.ocd/Vehicles.ocd/Lorry.ocd/Script.c
@@ -2,6 +2,9 @@
 
 #include Library_ElevatorControl
 
+static const LORRY_Effect_DumpContents = "DumpContents";
+static const LORRY_Effect_BlockCollection = "BlockCollectionByLorry";
+
 local content_menu;
 
 public func IsLorry() { return true;}
@@ -29,6 +32,124 @@ func Hit3()
 	Sound("DullMetalHit?");
 }
 
+/*-- Content Dumping --*/
+
+public func HoldingEnabled() { return true; }
+
+public func ControlUseStart(object clonk, int x, int y)
+{
+	var direction = DIR_Left;
+	if (x > 0)
+		direction = DIR_Right;
+	if (!GetEffect(LORRY_Effect_DumpContents, this))
+		AddEffect(LORRY_Effect_DumpContents, this, 100, 1, this, nil, direction);
+	return true;
+}
+
+public func ControlUseHolding(object clonk, int x, int y)
+{
+	var direction = DIR_Left;
+	if (x > 0)
+		direction = DIR_Right;
+	var effect = GetEffect(LORRY_Effect_DumpContents, this);
+	if (effect)
+		effect.dump_dir = direction;
+	return true;
+}
+
+public func ControlUseStop(object clonk, int x, int y)
+{
+	RemoveEffect(LORRY_Effect_DumpContents, this);
+	return true;
+}
+
+public func ControlUseCancel(object clonk, int x, int y)
+{
+	RemoveEffect(LORRY_Effect_DumpContents, this);
+	return true;
+}
+
+public func FxDumpContentsStart(object target, proplist effect, int temp, int direction)
+{
+	if (temp)
+		return FX_OK;
+	// The time it takes to dump the contents depends on the mass of the lorry.
+	effect.dump_strength = BoundBy(1000 / GetMass(), 3, 8);
+	effect.dump_dir = direction;
+	// Rotate the lorry into the requested direction.
+	var rdir = -effect.dump_strength;
+	if (effect.dump_dir == DIR_Right)
+		rdir = effect.dump_strength;
+	SetRDir(rdir);
+	return FX_OK;
+}
+
+public func FxDumpContentsTimer(object target, proplist effect, int time)
+{
+	// Rotate the lorry into the requested direction.
+	var rdir = -effect.dump_strength;
+	if (effect.dump_dir == DIR_Right)
+		rdir = effect.dump_strength;	
+	SetRDir(rdir);
+	// Dump one item every some frames if the angle is above 45 degrees. Only do this if the effect is at least active 
+	// for 10 frames to prevent an accidental click while holding the lorry to dump some of its contents.
+	if (time >= 10 && ((effect.dump_dir == DIR_Left && GetR() < -45) || (effect.dump_dir == DIR_Right && GetR() > 45)))
+	{
+		if (!Random(3))
+		{
+			var x = RandomX(6, 8) * Sign(GetR());
+			var xdir = RandomX(70, 90) * Sign(GetR());
+			var random_content = FindObjects(Find_Container(this), Sort_Random());
+			if (GetLength(random_content) >= 1)
+			{
+				random_content = random_content[0];
+				random_content->Exit(x, RandomX(2, 3), Random(360), 0, 0, RandomX(-5, 5));
+				random_content->SetXDir(xdir, 100);
+				// Assume the controller of the lorry is also the one dumping the contents.
+				random_content->SetController(GetController());
+				AddEffect(LORRY_Effect_BlockCollection, random_content, 100, 8, this);
+			}		
+		}
+	}
+	return FX_OK;
+}
+
+public func FxDumpContentsStop(object target, proplist effect, int reason, bool temp)
+{
+	if (temp)
+		return FX_OK;
+	// Stop rotating the lorry.
+	SetRDir(0);
+	return FX_OK;
+}
+
+public func FxBlockCollectionByLorryTimer() { return FX_Execute_Kill; }
+
+/*-- Rotation behaviour --*/
+
+protected func Initialize()
+{
+	AddEffect("IntUprightAttach", this, 1, 1, this);
+}
+
+
+protected func FxIntUprightAttachTimer(object target, proplist effect, int timer)
+{
+	// ignore this while dumping contents
+	var is_dumping = GetEffect(LORRY_Effect_DumpContents, target);
+	if (is_dumping) return FX_OK;
+
+	// rotate to upright position
+	var rot = target->GetR();
+	if (Abs(rot) < 30) return FX_OK;
+	if (!(GetContact(-1) & CNAT_Bottom)) return FX_OK;
+	if (GetContact(2) && GetContact(3)) return FX_OK;
+
+	var rdir_desired = -rot / 5;
+	var rdir_current = target->GetRDir();
+	target->SetRDir(rdir_current + BoundBy(rdir_desired - rdir_current, -5, 5));
+}
+
 /*-- Contents --*/
 
 private func MaxContentsCount()
@@ -38,6 +159,9 @@ private func MaxContentsCount()
 
 protected func RejectCollect(id object_id, object obj)
 {
+	// objects that cannot be collected right now
+	if (GetEffect(LORRY_Effect_BlockCollection, obj)) return true;
+
 	// objects can still be collected
 	if (ContentsCount() < this->MaxContentsCount())
 	{
@@ -87,13 +211,12 @@ local ActMap = {
 			Wdt = 22,
 			Hgt = 16,
 			NextAction = "Drive",
-			//Animation = "Drive",
 		},
 };
 
 func Definition(def)
 {
-	SetProperty("PictureTransformation",Trans_Mul(Trans_Rotate(-25,1,0,0),Trans_Rotate(40,0,1,0)),def);
+	def.PictureTransformation = Trans_Mul(Trans_Rotate(-25,1,0,0),Trans_Rotate(40,0,1,0));
 }
 
 local Name = "$Name$";
