44187e976cf89802fefd180a77a3f8b45c74488f
Mark <markh4b@hotmail.com>
2016-01-26 22:53:26 +0100

    Knowledge stuff by Maikel

---
+++ Classic.ocf/Classic.ocd/System.ocg/Script_Knowledge.c
@@ -0,0 +1,46 @@
+/**
+	Knowdlege
+	Provides global functions to give players knowledge for scenarios.
+	
+	@author Maikel
+*/
+
+ 
+// Gives the player specific knowledge as given in the knowledge array.
+global func GivePlayerSpecificKnowledge(int plr, array knowledge)
+{
+	for (var plan in knowledge)
+		SetPlrKnowledge(plr, plan);
+}
+
+// Removes the player specific knowledge as given in the knowledge array.
+global func RemovePlayerSpecificKnowledge(int plr, array knowledge)
+{
+	for (var plan in knowledge)
+		SetPlrKnowledge(plr, plan, true);
+}
+
+// Gives the player plans according to basic knowledge.
+global func GivePlayerBasicKnowledge(int plr)
+{
+	var knowledge = [
+		// Basic structures for a settlement and production of tools and explosives.
+		Flagpole, Basement, WindGenerator, SteamEngine, Compensator, Sawmill, Foundry, Elevator, ToolsWorkshop, ChemicalLab, Chest,
+		// Basic tools for mining and tree chopping and loam production.
+		Shovel, Hammer, Axe, Pickaxe, Barrel, Bucket, Torch,
+		// The basic resources.
+		Metal, Loam, GoldBar,
+		// Some of the basic explosives.
+		Dynamite, DynamiteBox,
+		// Some basic vehicles which aid in the settlement construction.
+		Lorry
+	];
+	for (var plan in knowledge)
+		SetPlrKnowledge(plr, plan);
+	return;
+}
+
+global func GivePlayerPowerKnowledge(int plr, bool ignore_power_producers)
+{
+	var knowledge = [ClassicWindmill, ClassicPowerPlant];
+}
