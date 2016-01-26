f89e83c6f2b41e46ddc0367d9e897fb4672db67a
Mark <markh4b@hotmail.com>
2016-01-26 22:35:59 +0100

    T-Flint components, script, graphics

--- Classic.ocf/Classic.ocd/Items.ocd/Weapons.ocd/TFlint.ocd/Script.c
+++ Classic.ocf/Classic.ocd/Items.ocd/Weapons.ocd/TFlint.ocd/Script.c
@@ -1,21 +1,79 @@
-/*--- Flint ---*/
+/*--- T-Flint ---*/
 
 protected func Construction()
 {
 	var graphic = Random(3);
-	if(graphic)
-		SetGraphics(Format("%d",graphic));
+	if (graphic) SetGraphics(Format("%d",graphic));
 }
 
-protected func Hit()
+public func ControlUse(object clonk, int x, int y)
 {
-  Explode(ExplodeSize());
+	// if already activated, nothing (so, throw)
+	if (GetEffect("FuseBurn", this))
+	{
+		clonk->ControlThrow(this, x, y);
+		return true;
+	}
+	else
+	{
+		Fuse();
+		return true;
+	}
 }
 
-public func ExplodeSize() 
-{ 
-  return(18); 
+func Fuse()
+{
+	if (!GetEffect("FuseBurn"))
+	{
+		AddEffect("FuseBurn", this, 1, 1, this);
+		SetGraphics("Active");
+	}
 }
+
+func FxFuseBurnTimer(object bomb, proplist effect, int timer)
+{
+	//Sound("Fire::Spark*");
+	CreateParticle("Fire", 0, 0, PV_Random(-5, 5), PV_Random(-15, 5), PV_Random(10, 40), Particles_Glimmer(), 5);
+
+	if (timer == 1)
+	{
+		Sound("Fire::Fuse");
+	}
+	if(timer >= 60)
+	{
+		Sound("Fire::FuseLoop",nil,nil,nil,-1);
+		DoExplode();
+		return FX_Execute_Kill;
+	}
+}
+
+func DoExplode()
+{
+	Explode(this->ExplodeSize());
+}
+
+func ExplodeSize()
+{
+	return 20;
+}
+
+protected func Hit(int x, int y)
+{
+	StonyObjectHit(x,y);
+	Fuse();
+}
+
+protected func RejectEntrance()
+{
+	return GetAction() == "Fuse" || GetAction() == "Ready";
+}
+
+// Drop fusing bomb on death to prevent explosion directly after respawn
+public func IsDroppedOnDeath(object clonk)
+{
+	return !!GetEffect("FuseBurn", this);
+}
+
 
 local Collectible = 1;
 local Name = "$Name$";
