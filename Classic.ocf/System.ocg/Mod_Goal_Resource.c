75790162edf0569a6e29b50a75342776ad6758c5
Mark <markh4b@hotmail.com>
2016-01-26 20:14:49 +0100

    Resource can be mined by percentage now

---
+++ Classic.ocf/System.ocg/Mod_Goal_Resource.c
@@ -0,0 +1,13 @@
+#appendto Goal_ResourceExtraction
+
+public func SetResource(string resource, int percent)
+{
+	if (!percent) percent = 100;
+
+	var pos = GetLength(resource_list);
+	resource_list[pos] = resource;
+	var mat_cnt = GetMaterialCount(Material(resource)) * percent / 100;
+	var blast_ratio = GetMaterialVal("Blast2ObjectRatio", "Material", Material(resource));
+	tolerance_list[pos] = Max(1, mat_cnt / blast_ratio / 20);
+	return;
+}
