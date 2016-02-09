bf12314212ce5aa5c483e8bc2f572482cc0b0790
Mark <markh4b@hotmail.com>
2016-02-09 18:42:11 +0100

    Shovel: Adjusted dig speed callback to the latest version

--- Classic.ocf/Classic.ocd/System.ocg/Mod_Shovel.c
+++ Classic.ocf/Classic.ocd/System.ocg/Mod_Shovel.c
@@ -1,11 +1,7 @@
 #appendto Shovel
 
 
-func GetModifiedDigSpeed(object clonk, int speed)
+func GetDigSpeed(object clonk)
 {
-//	var iAnimation = GetEffect("IntDig", clonk).var1;
-//	var iPosition = clonk->GetAnimationPosition(iAnimation)*180/clonk->GetAnimationLength("Dig");
-//	speed = speed*(Cos(iPosition-45, 50)**2)/2500;
-
-	return 3 * speed / 4;
+	return 3 * clonk.ActMap.Dig.Speed / 2;
 }
