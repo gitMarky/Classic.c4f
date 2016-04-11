4710bd93f882c02f521279baf662d07c5b2f9349
Mark <markh4b@hotmail.com>
2016-04-11 06:46:45 +0200

    Bugfix: Less normal trees in Ashlands

--- Classic.ocf/Worlds.ocf/Ashlands.ocs/System.ocg/Tree.c
+++ Classic.ocf/Worlds.ocf/Ashlands.ocs/System.ocg/Tree.c
@@ -2,18 +2,32 @@
 
 local dead = false;
 
+// Prevent tree movement
 func StartAnimation()
 {
 	if (GetAction() == "Chopped") return;
 	_inherited();
 }
 
-// Prevent tree movement
 func SetBurned()
 {
 	dead = true;
 	SetGraphics("Burned");
 	SetAction("Chopped");
 }
+
+func Construction()
+{
+	_inherited(...);
+	
+	// new normal trees should be extremely rare in Ashlands
+	// other burned trees may grow though, even if this makes 
+	// no sense => you need some wood sometimes
+	if (Random(50))
+	{
+		SetBurned();
+	}
+}
+
 
 func IsDeadTree(){ return dead; }
