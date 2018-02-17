9803c6d1ea01c61a7ba629075a4cec53022b4a9c
Mark <markh4b@hotmail.com>
2018-02-17 19:36:19 +0100

    Ambience sounds: Docu

--- Classic.ocf/Classic.ocd/Effects.ocd/AmbienceSound.ocd/Script.c
+++ Classic.ocf/Classic.ocd/Effects.ocd/AmbienceSound.ocd/Script.c
@@ -6,9 +6,12 @@
 	@author Marky
 */
 
+/* -- Internals and engine callbacks -- */
+
 local Name = "$Name$";
 local Description = "$Description$";
 local Visibility = VIS_Editor;
+
 
 private func Construction()
 {
@@ -44,17 +47,93 @@ local FxAmbience = new Effect
 };
 
 
+/* -- Interface -- */
+
+/**
+ Sets the chance for sounds to occur.
+ 
+ @par change Once every 'interval' frames, the object tries
+             playing a sound - but only 'chance'% of the
+             time.
+             Passing 'nil' will reset the interval
+             to the default value of 13%, which is roughly
+             1/8 times.
+
+ @return object This object, so that you can issue further
+                function calls conveniently.
+ */
 public func SetChance(int chance)
 {
 	var fx = GetEffect("FxAmbience", this);
 	if (fx)
 	{
-		fx.Chance = chance;
+		fx.Chance = chance ?? 13;
 	}
 	return this;
 }
 
 
+/**
+ Sets an icon, so that you can distinguish the sounds in the editor view.
+ */
+public func SetIcon(id icon)
+{
+	SetGraphics(nil, icon, 1, GFXOV_MODE_IngamePicture);
+	SetObjDrawTransform(1000, 0, GetDefWidth() * 300, 0, 1000, GetDefHeight() * 200, 1);
+	return this;
+}
+
+
+/**
+ Sets the interval for sounds.
+ 
+ @par interval The interval between sounds playing, in frames.
+               Passing 'nil' will reset the interval
+               to the default value of 30 frames.
+               
+ @return object This object, so that you can issue further
+                function calls conveniently.
+ */
+public func SetInterval(int interval)
+{
+	var fx = GetEffect("FxAmbience", this);
+	if (fx)
+	{
+		fx.Interval = interval ?? 30;
+	}
+	return this;
+}
+
+
+/**
+ Sets the sound options for the sound.
+ 
+ @par options A proplist with options for the Sound() function.
+              
+              For example, SetOptions({volume = 50}) will play
+              the sounds at 50% volume.
+
+ @return object This object, so that you can issue further
+                function calls conveniently.
+ */
+public func SetOptions(proplist options)
+{
+	var fx = GetEffect("FxAmbience", this);
+	if (fx)
+	{
+		fx.SoundOptions = options;
+	}
+	return this;
+}
+
+
+/**
+ Sets the sound that should be played.
+ 
+ @par name The sound name, as if played by the Sound() function.     
+ @return object This object, so that you can issue further
+                function calls conveniently.
+ */
 public func SetSound(string name)
 {
 	var fx = GetEffect("FxAmbience", this);
@@ -66,36 +145,21 @@ public func SetSound(string name)
 }
 
 
-public func SetOptions(proplist opts)
-{
-	var fx = GetEffect("FxAmbience", this);
-	if (fx)
-	{
-		fx.SoundOptions = opts;
-	}
-	return this;
-}
-
-
-public func SetIcon(id icon)
-{
-	SetGraphics(nil, icon, 1, GFXOV_MODE_IngamePicture);
-	SetObjDrawTransform(1000, 0, GetDefWidth() * 300, 0, 1000, GetDefHeight() * 200, 1);
-	return this;
-}
-
-
-public func SetInterval(int interval)
-{
-	var fx = GetEffect("FxAmbience", this);
-	if (fx)
-	{
-		fx.Interval = interval;
-	}
-	return this;
-}
-
-
+/**
+ Sets a condition when the sound should be played.
+ 
+ @par condition A function or function name for a condition.
+                This function is called by the ambience sound
+                object, so it either has to be a global function
+                or one that the ambience sound object has.
+                Sounds are played only if the condition returns true.
+                
+                For example, SetCondition(Global.IsNight) will result
+                in sounds playing only at night.
+                
+ @return object This object, so that you can issue further
+                function calls conveniently.
+ */
 public func SetCondition(condition)
 {
 	var fx = GetEffect("FxAmbience", this);
@@ -107,6 +171,13 @@ public func SetCondition(condition)
 }
 
 
+/**
+ Wait at least this many frames before the (next) sound starts playing.
+ 
+ @par frames The amount of frames to wait.
+ @return object This object, so that you can issue further
+                function calls conveniently.
+ */
 public func Wait(int frames)
 {
 	var fx = GetEffect("FxAmbience", this);
