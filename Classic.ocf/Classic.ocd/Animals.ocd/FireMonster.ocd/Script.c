e687e7ad8ae87525d2521b69712d183f3dec7f8a
Mark <markh4b@hotmail.com>
2018-02-21 22:31:00 +0100

    Fire monster: Directed fire attack
    Fire monsters attack prey

--- Classic.ocf/Classic.ocd/Animals.ocd/FireMonster.ocd/Script.c
+++ Classic.ocf/Classic.ocd/Animals.ocd/FireMonster.ocd/Script.c
@@ -27,21 +27,29 @@ public func StartFlame()
 	Sound("Fire::Inflame");
 	Sound("Animals::Monster::MonsterGrowl?");
 	PlayAnimation("Breath", ANIM_SLOT_Monster_Attack, Anim_Linear(0, 0, GetAnimationLength("Breath"), 40, ANIM_Remove));
+	SetAction("FireBreath"); // So that it does not walk
 }
 
 
-private func Flaming()
+private func Flaming(int target_angle)
 {
-	var x = 22 * GetCalcDir();
-	var y = -2;
+	var pos = GetAttackPos();
 	
-	var angle = 90 * GetCalcDir() + RandomX(-7, 7);
-	var range = RandomX(30, 40);
+	var angle = Normalize(target_angle ?? (90 * GetCalcDir()), -180);
+	angle -= GetCalcDir() * 10;
+	angle += RandomX(-7, 7);
+	var range = RandomX(40, 60);
 
-	var flame = CreateObject(Flame, x, y, GetOwner());
+	var flame = CreateObject(Flame, pos.x, pos.y, GetOwner());
 	flame->SetController(GetController());
 	flame->SetXDir(+Sin(angle, range));
 	flame->SetYDir(-Cos(angle, range));
+}
+
+
+private func GetAttackPos()
+{
+	return { x = 22 * GetCalcDir() * GetCon() / 100, y = (-2) * GetCon() / 100, };
 }
 
 
@@ -58,7 +66,26 @@ local FxFireAttack = new Effect
 		// Actual flame attack every 2 frames
 		if (Inside(time, 15, 35) && time%2)
 		{
-			this.Target->Flaming();
+			// Find a victim
+			if (!this.Prey)
+			{
+				var x_range = 2 * this.Target->GetCon();
+				var y_range = this.Target->GetCon();
+				var dir = this.Target->GetDir();
+				this.Prey = this.Target->FindObject(Find_Func("IsPrey"), Find_OCF(OCF_Alive), 
+				                                    Find_InRect((-1 + dir) * x_range, -y_range, x_range, 2 * y_range));
+			}
+			// Spray it!!!!
+			if (this.Prey)
+			{
+				var pos = this.Target->GetAttackPos();
+				var angle = Angle(this.Target->GetX() + pos.x, this.Target->GetY() + pos.y, this.Prey->GetX(), this.Prey->GetY()); 
+				this.Target->Flaming(angle);
+			}
+			else
+			{
+				this.Target->Flaming();
+			}
 		}
 		return FX_OK;
 	},	
@@ -77,3 +104,19 @@ local ContactCalls = true;
 local Collectible = true;
 local BorderBound = C4D_Border_Sides;
 
+local ActMap = {
+
+FireBreath = {
+	Prototype = Action,
+	Name = "FireBreath",
+	Procedure = DFA_NONE,
+	Directions = 2,
+	FlipDir = 1,
+	Delay = 40,
+	Attach = 8,
+	FacetBase = 1,
+	NextAction = "Walk",
+	InLiquidAction = "Swim",
+},
+
+};
