c2b1a3e8345fb42e6b4ee87d8839a128d06f4b48
Mark <markh4b@hotmail.com>
2018-02-21 21:29:34 +0100

    Monster: Firemonster

---
+++ Classic.ocf/Classic.ocd/Animals.ocd/FireMonster.ocd/Script.c
@@ -0,0 +1,16 @@
+/* -- Fire monster -- */
+
+#include Monster
+
+/* -- Properties -- */
+
+local Name = "$Name$";
+local Description = "$Description$";
+
+local MaxEnergy = 250000;
+local MaxBreath = 720; // Monster can breathe for 20 seconds under water.
+local NoBurnDecay = 1;
+local ContactCalls = true;
+local Collectible = true;
+local BorderBound = C4D_Border_Sides;
+
